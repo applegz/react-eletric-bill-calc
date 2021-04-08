2e1dbceb4a697ee52432493909a477a0b8530d16
Guoying Zhong <ffamochong@gmail.com>
2021-04-07 23:17:21 -0400

    add onChange and onSumbit to form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Form(props) {
+  const [rate, setRate] = useState('RateA');
+  const [miles, setMiles] = useState(0);
+  const [chargeTime, setChargeTime] = useState('tou1');
+
   return (
     <div>
       Form
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(rate, miles, chargeTime);
+        }}
+      >
         <label>What rate you currently on?</label>
-        <select>
+        <select value={rate} onChange={(e) => setRate(e.target.value)}>
           <option value="RateA">Flat Rate</option>
           <option value="RateB">TOU Rate</option>
         </select>
         <label>How many miles do you expect to drive per year?</label>
-        <input type="number" placeholder="100" min="0" required></input>
+        <input
+          type="number"
+          min="1"
+          onChange={(e) => setMiles(e.target.value)}
+          value={miles}
+          required
+        ></input>
         <label>When do you plan to charge your car?</label>
-        <select>
+        <select
+          value={chargeTime}
+          onChange={(e) => setChargeTime(e.target.value)}
+        >
           <option value="tou1">12 pm - 6 pm</option>
           <option value="tou2">any other time</option>
         </select>
-        <input type="submit" value="Submit"></input>
+        <input type="submit" value="Submit" disabled={!miles}></input>
       </form>
     </div>
   );
